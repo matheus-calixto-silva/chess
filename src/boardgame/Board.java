fac554a4c35bc37f4e5cd8fbc653b4add7b87bcb
Matheus Calixto <matheuscalixto8@gmail.com>
2020-10-27 13:32:18 -0300

    Movimentacao de pecas

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,6 +45,21 @@ public class Board {
 		 pieces[position.getRow()][position.getColumn()] = piece;
 		 piece.position = position; 
 	 }
+	 
+	 public Piece removePiece(Position position) {
+		 if(!positionExists(position)) {
+			 throw new BoardException("Posição não existe no tabuleiro");
+		 }		
+		 
+		 if(piece(position) == null) {
+			 return null;
+		 }
+		 
+		 Piece aux = piece(position);
+		 aux.position = null;
+		 pieces[position.getRow()][position.getColumn()] = null;
+		 return aux;
+	 }
 
 	 private boolean positionExists(int row, int column) {
 		   return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -56,7 +71,7 @@ public class Board {
 	 
 	 public boolean thereIsAPiece(Position position) {
 		 if(!positionExists(position)) {
-			 throw new BoardException("Posição não existem no tabuleiro");
+			 throw new BoardException("Posição não existe no tabuleiro");
 		 } 
 		return piece(position) != null;
 	 }
