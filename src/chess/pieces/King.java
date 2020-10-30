cfe3528cba440b3aedacf82967d1d5bf08efc36f
Matheus Calixto <matheuscalixto8@gmail.com>
2020-10-30 15:38:15 -0300

    Implementaçao da jogada especial roque

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,79 +2,112 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-	public King(Board board, Color color) {
+	private ChessMatch chessMatch;
+
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
 	public String toString() {
 		return "K";
 	}
-	
+
 	private boolean canMove(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
+	}
+
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
-		
+
 		Position p = new Position(0, 0);
-		
-		//acima
+
+		// acima
 		p.setValues(position.getRow() - 1, position.getColumn());
-		if(getBoard().positionExists(p) && canMove(p)) {
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//esquerda
+
+		// esquerda
 		p.setValues(position.getRow(), position.getColumn() - 1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//direita
+
+		// direita
 		p.setValues(position.getRow(), position.getColumn() + 1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//abaixo
+
+		// abaixo
 		p.setValues(position.getRow() + 1, position.getColumn());
-		if(getBoard().positionExists(p) && canMove(p)) {
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//diagonal superior esquerda
-		p.setValues(position.getRow() -1, position.getColumn() -1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+
+		// diagonal superior esquerda
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//diagonal superior direita
-		p.setValues(position.getRow() -1, position.getColumn() +1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+
+		// diagonal superior direita
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//diagonal inferior esquerda
-		p.setValues(position.getRow() +1, position.getColumn() -1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+
+		// diagonal inferior esquerda
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		//diagonal inferior direita
-		p.setValues(position.getRow() +1, position.getColumn() +1);
-		if(getBoard().positionExists(p) && canMove(p)) {
+
+		// diagonal inferior direita
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
-		
-		return mat;
+
+		// Movimento especial roque
+		if (getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// roque do lado do rei
+			Position postT1 = new Position(position.getRow(), position.getColumn() + 3);
+			if (testRookCastling(postT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+
+				if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+			}
+			// roque do lado da rainha
+			Position postT2 = new Position(position.getRow(), position.getColumn() - 4);
+			if(testRookCastling(postT1)) {
+				Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+				Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+				Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+				if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+					}
+				}
+			}
+
+			return mat;
+		}
 	}
-}
